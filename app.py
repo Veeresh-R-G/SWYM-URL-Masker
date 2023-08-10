5b4fbcb35d38b2921542431330c345dc6a217756
Veeresh R G <veeresh.ravi@gmail.com>
2023-08-10 15:31:43 +0530

    UI Improvements

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-from flask import Flask, request, redirect, render_template , render_template_string
+from flask import Flask, request, redirect, render_template , send_file
 import csv
 import os
 import sqlite3
 import secrets
 import webbrowser
+import random
+import string
+import pandas as pd
+import openpyxl
 app = Flask(__name__)
 
 # Dictionary to store masked links
 '''
     Key = Masked Link
-    Value->List = Original Link , Click Count
+    Value->List = Original Link , Click Count , random combination
 '''
 link_mask = {}
-
+flag = False
 # Set up SQLite database for storing click data
 db_conn = sqlite3.connect("click_data.db")
 db_cursor = db_conn.cursor()
@@ -54,8 +58,13 @@ def index():
             for row in csv.reader(csv_rows):
                 original_link = (row[0])  
                 masked_link = generate_masked_link(original_link)
-                print("Here")
-                link_mask[masked_link] = [original_link, 0]
+                print("Masked Link : ", masked_link)
+                N = 10
+                res = ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
+                
+                print(masked_link)
+                link_mask[masked_link] = [original_link, 0 , res]
+
                 # insert_query = "INSERT OR IGNORE INTO clicks (masked_link, original_link) VALUES (?, ?)"
                 # db_cursor.execute(insert_query, (masked_link, original_link))
                 # db_cursor.execute("INSERT OR IGNORE INTO clicks (masked_link, original_link) VALUES (?, ?)", (masked_link, original_link))
@@ -63,6 +72,7 @@ def index():
                 print("Inserted Record : ", masked_link, original_link)
             # db_conn.commit()
         return render_template("Dashboard.html", link_mask=link_mask)
+    link_mask.clear()
     return render_template("index.html")
 
 @app.route("/masked_link/<masked_link>" , methods=["GET"])
@@ -81,7 +91,14 @@ def redirect_to_original(masked_link):
     
     # return to the dashboard html route
     return render_template("Dashboard.html", link_mask=link_mask)
-    return "Hittin this route"
+
+@app.route("/export-excel")
+def export_to_excel():
+    rows = [(key, values[0], values[1]) for key, values in link_mask.items()]
+    df = pd.DataFrame(rows, columns=['Masked Link', 'Original Link', '# of Opens'])
+    excel_file = "exported_data.xlsx"
+    df.to_excel(excel_file, index=False)
+    return send_file(excel_file, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
