134c888f1e5c63479c8608dfff940e930fe27e42
Veeresh R G <veeresh.ravi@gmail.com>
2023-08-10 12:29:31 +0530

    [ADD] DB CONN

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, request, redirect, render_template
-# import csv
-# import os
-# import sqlite3
-# import secrets
+import csv
+import os
+import sqlite3
+import secrets
 
 app = Flask(__name__)
 
-# Set up SQLite database for storing click data
+#Connection to SQLITE DB and creating a table If not exists
+db_conn = sqlite3.connect("db/click_data.db")
+db_cursor = db_conn.cursor()
+db_cursor.execute("""
+    CREATE TABLE IF NOT EXISTS clicks (
+        masked_link TEXT,
+        original_link TEXT,
+        click_count INTEGER DEFAULT 0,
+        PRIMARY KEY (masked_link)
+    )
+""")
+db_conn.commit()
+
+print("DB Connection Established Successfully")
 
 @app.route("/",methods=["GET","POST"])
 def helloWorld():
